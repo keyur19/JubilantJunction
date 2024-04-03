55ca86a879ec2b8668af6adad6ebab71775dfff3
keyur19 <142803070+keyur19@users.noreply.github.com>
2024-04-03 22:26:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeColor() {
+    var element = document.querySelector(".navbar-nav .nav-item");
+    element.style.backgroundColor = 'rgb(247, 224, 228)'; // Change background color to blue
+    console.log("hello");
+  }
